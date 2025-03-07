4de03c001f0fced64530a2f768c23c535116533a
MichaelKazembe <michaelkazembe@gmail.com>
2025-03-07 21:27:20 +0200

    Find sum, average and max numbers from user input

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number;
+        List<int> numbers = new List<int>();
+
+        Console.Write("Enter a series numbers. Enter 0 to stop: ");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number !=0)
+            {
+                numbers.Add(number);
+                Console.WriteLine($"You entered {number}");
+            }  
+
+        }
+        while (number != 0);
+
+        int sum = 0; 
+        int  max = int.MinValue;    
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        double average = (double)sum / numbers.Count; 
+
+        Console.WriteLine($"The sum of numbers: {sum}");
+        Console.WriteLine($"The average numbe: {average}");
+        Console.WriteLine($"The largest number: {max}");
     }
 }
