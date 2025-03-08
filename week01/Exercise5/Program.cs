92e271829503b117a2bc4e2c5c8d261b91493f1d
MichaelKazembe <michaelkazembe@gmail.com>
2025-03-08 22:46:54 +0200

    Create and call functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,7 +3,48 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    {       
+       
+        DisplayWelcome();
+        
+        string userName = "";
+        userName = PromptUserName(userName);
+
+        int userNumber = 0;
+        userNumber = UserNumber(userNumber);
+
+        userNumber = SquareNumber(userNumber);
+        DisplayResult(userName, userNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(string userName)
+    {
+        Console.Write("Please enter your name:");
+        userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int UserNumber(int userNumber)
+    {
+        Console.Write("Please enter your favorite number:");
+        userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+
+    static int SquareNumber(int userNumber)
+    {
+        return userNumber * userNumber;
+    }
+
+    static void DisplayResult(string userName, int userNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {userNumber}.");
+    }
+
 }
