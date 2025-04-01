53149de6846accc49a90399b0bc2d2526c420705
MichaelKazembe <michaelkazembe@gmail.com>
2025-04-01 12:48:00 +0200

    Create math assignment class

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -7,8 +7,12 @@ class Program
         // Instantiate an object of the Assignment class
 
         Assignment assignment1= new Assignment("Michael", "C#");
+        MathAssignment mathAssignment1 = new MathAssignment("Michael", "C#", "7.3", "8-19");
 
         // Call the GetSummary method
         Console.WriteLine(assignment1.GetSummary());
+
+        // Call the GetHomeworkList method
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
     }
 }
